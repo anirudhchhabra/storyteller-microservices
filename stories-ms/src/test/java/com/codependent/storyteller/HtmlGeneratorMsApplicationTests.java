2053206171b429c9c31a1fcf5edcd623e0788a32
Anirudh Chhabra <anirudh.chhabra@publicissapient.com>
2019-12-01 20:41:11 +0530

    Upgraded Spring Cloud version

--- stories-ms/src/test/java/com/codependent/storyteller/HtmlGeneratorMsApplicationTests.java
+++ stories-ms/src/test/java/com/codependent/storyteller/HtmlGeneratorMsApplicationTests.java
@@ -2,13 +2,11 @@ package com.codependent.storyteller;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.codependent.storyteller.HtmlGeneratorMsApplication;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = HtmlGeneratorMsApplication.class)
+@SpringBootTest(classes = HtmlGeneratorMsApplication.class)
 public class HtmlGeneratorMsApplicationTests {
 
 	@Test
