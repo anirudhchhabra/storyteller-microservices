43c167f4fd7f0f1dcef721083ff8315a11d9e5d3
Anirudh Chhabra <anirudh.chhabra@publicissapient.com>
2019-12-01 20:49:54 +0530

    Resolved dependencies

--- storyteller-api/src/main/java/com/codependent/storyteller/StoryTellerApiApplication.java
+++ storyteller-api/src/main/java/com/codependent/storyteller/StoryTellerApiApplication.java
@@ -11,7 +11,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
