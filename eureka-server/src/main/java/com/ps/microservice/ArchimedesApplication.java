f1f10990a179394d3da5b4d203f5ee6ba783faec
Anirudh Chhabra <anirudh.chhabra@publicissapient.com>
2019-12-01 21:01:43 +0530

    Refactored Package Name

--- eureka-server/src/main/java/com/ps/microservice/ArchimedesApplication.java
+++ eureka-server/src/main/java/com/ps/microservice/ArchimedesApplication.java
@@ -1,4 +1,4 @@
-package com.codependent.storyteller;
+package com.ps.microservice;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
--- eureka-server/src/test/java/com/ps/microservice/ArchimedesApplicationTests.java
+++ eureka-server/src/test/java/com/ps/microservice/ArchimedesApplicationTests.java
@@ -1,9 +1,11 @@
-package com.codependent.storyteller;
+package com.ps.microservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.ps.microservice.ArchimedesApplication;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 //@RunWith(SpringRunner.class)
